cdd7f6284855f19e593da097bfdb402fea4ad8e2
Vadim Baryshev <vadik.barysheff@gmail.com>
2024-04-25 15:22:17 +0500

    create_gitignore

--- attestation/obj/Debug/net8.0/attestation.AssemblyInfo.cs
+++ attestation/obj/Debug/net8.0/attestation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("attestation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+325a678ae0e8c29bbdfbbf28115e920329f466ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+639678b0559d2ce9f0e78f82f9eba139b287c125")]
 [assembly: System.Reflection.AssemblyProductAttribute("attestation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("attestation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
